37ff6319ccb2faff1e793ffc61bef45b222345b1
Pawel Lipinski <lipinskijob@gmail.com>
2024-02-13 16:07:08 +0100

    Respond to PING

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,8 +9,18 @@ public class Main {
         try (final var serverSocket = new ServerSocket(port)) {
             serverSocket.setReuseAddress(true);
             final var clientSocket = serverSocket.accept();
+            responsePong(clientSocket);
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void responsePong(Socket socket) {
+        try (var writer = new PrintWriter(socket.getOutputStream(), true)) {
+            writer.print("+PONG\r\n");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
             throw new RuntimeException(e);
         }
     }
