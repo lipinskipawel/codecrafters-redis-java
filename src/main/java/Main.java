b9edb6ac664d8e705e201bd07362c41762a1d4e2
Pawel Lipinski <lipinskijob@gmail.com>
2024-02-14 07:14:11 +0100

    Respond to multiple PINGs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,38 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
         final int port = 6379;
         try (final var serverSocket = new ServerSocket(port)) {
             serverSocket.setReuseAddress(true);
-            final var clientSocket = serverSocket.accept();
-            responsePong(clientSocket);
+
+            // closing socket closes input and output streams
+            try (final var clientSocket = serverSocket.accept()) {
+                final var reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                final var writer = new PrintWriter(clientSocket.getOutputStream());
+                String command;
+                while ((command = reader.readLine()) != null) {
+                    System.out.println("Command: " + command);
+                    if (command.equalsIgnoreCase("PING")) {
+                        responsePong(writer);
+                    }
+                }
+            } catch (Exception exception) {
+                System.out.println("Exception by the accept method has been thrown: " + exception.getMessage());
+                throw new RuntimeException(exception);
+            }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
             throw new RuntimeException(e);
         }
     }
 
-    private static void responsePong(Socket socket) {
-        try (var writer = new PrintWriter(socket.getOutputStream(), true)) {
-            writer.print("+PONG\r\n");
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            throw new RuntimeException(e);
-        }
+    private static void responsePong(PrintWriter writer) {
+        writer.print("+PONG\r\n");
+        writer.flush();
     }
 }
